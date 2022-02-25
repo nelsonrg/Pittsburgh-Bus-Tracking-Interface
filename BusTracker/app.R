86a179c81da2a2d1ae7b53cd72ea4ffd95225433
Bobby Nelson <rnelsonxy@gmail.com>
2022-02-24 21:09:54 -0500

    Adds an input selection for the bus route to be plotted.

--- BusTracker/app.R
+++ BusTracker/app.R
@@ -10,13 +10,13 @@ library(xml2)
 key <- readChar("api_key.txt", file.info("api_key.txt")$size)
 
 # base url for the api calls
-base_url <- "http://realtime.portauthority.org/bustime/api/v3/getvehicles"
+base_url <- "http://realtime.portauthority.org/bustime/api/v3"
 
 # function to query bus data from api
 # following from: https://urbandatapalette.com/post/2021-03-xml-dataframe-r/
 getBusData <- function(value, type) {
     # add key to request
-    url <- paste0(base_url, "?key=", key)
+    url <- paste0(base_url, "/getvehicles?key=", key)
     
     # add either route or vehicle id to request
     if (type == "route") {
@@ -38,6 +38,26 @@ getBusData <- function(value, type) {
         readr::type_convert() 
 }
 
+# get the route information when the app launches
+getRouteData <- function() {
+    # add key to request
+    url <- paste0(base_url, "/getroutes?key=", key)
+    
+    request <- RETRY("GET", URLencode(url), repeated=TRUE)
+    content <- content(request, "text")
+    
+    # parse xml
+    xml <- as_list(read_xml(content))
+    
+    bustime_df <- as_tibble(xml) %>%
+        unnest_wider(`bustime-response`) %>%
+        unnest(cols = names(.)) %>%
+        readr::type_convert() %>%
+        # remove non-bus info
+        filter(rtpidatafeed == "Port Authority Bus")
+}
+route.data <- getRouteData()
+
 # define custom icons for the map
 icons <- awesomeIconList(
     bus.simple = makeAwesomeIcon(icon="bus-simple", library="fa")
@@ -47,17 +67,28 @@ icons <- awesomeIconList(
 ui <- navbarPage(
 
     # Application title
-    titlePanel("Pittsburgh Bus Tracker"),
+    "Pittsburgh Bus Tracker",
     
     # bus map
     tabPanel("Live Map",
-             mainPanel(
-                 shinyjs::useShinyjs(),
-                 tags$style(type = "text/css", ".leaflet {height: calc(100vh - 90px) !important;}
-                                         body {background-color: #D4EFDF;}"),
-                 # Map Output
-                 leafletOutput("leaflet")
-                )
+             # input
+             sidebarLayout(
+                 sidebarPanel(
+                     selectInput(inputId="route.select",
+                                 label="Route Number",
+                                 choices=unique(route.data$rtdd),
+                                 selected=unique(route.data$rtdd)[1])
+                 ),
+             # map
+                 mainPanel(
+                     shinyjs::useShinyjs(),
+                     tags$style(type = "text/css", 
+                                ".leaflet {height: calc(100vh - 90px) !important;}
+                                body {background-color: #D4EFDF;}"),
+                     # Map Output
+                     leafletOutput("leaflet")
+                    )
+                 )
              ),
     
     # top bar menu
@@ -73,7 +104,8 @@ server <- function(input, output) {
     
     # updating vehicle location data
     bus.data <- reactive({
-        getBusData(value=2, type="route")
+        req(input$route.select)
+        getBusData(value=input$route.select, type="route")
     })
 
     # raw data table for display
@@ -89,18 +121,27 @@ server <- function(input, output) {
     # bus coordinate view
     # only update when bus data updates
     observe({
-        data <- bus.data() %>%
-            mutate(lat = as.numeric(lat),
-                   lon = as.numeric(lon))
-        
-        leafletProxy("leaflet", data=data) %>%
-            clearGroup(group="busPosition") %>%
-            addAwesomeMarkers(lng=~lon,
-                              lat=~lat,
-                              icon=makeAwesomeIcon(icon="bus", library="fa"),
-                              popup=~vid,
-                              group="busPosition",
-                              layerId=~vid)
+        # if no buses, clear the markers and then exit
+        if (!("lat" %in% colnames(bus.data()))) {
+            leafletProxy("leaflet") %>%
+                clearGroup(group="busPosition")
+        } else {
+            # convert lat and lon for plotting
+            data <- bus.data() %>%
+                mutate(lat = as.numeric(lat),
+                       lon = as.numeric(lon))
+            
+            
+            
+            leafletProxy("leaflet", data=data) %>%
+                clearGroup(group="busPosition") %>%
+                addAwesomeMarkers(lng=~lon,
+                                  lat=~lat,
+                                  icon=makeAwesomeIcon(icon="bus", library="fa"),
+                                  popup=~vid,
+                                  group="busPosition",
+                                  layerId=~vid)
+        }
     })
 }
 
