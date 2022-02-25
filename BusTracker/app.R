3890dd2ddb908c97bc0ef8c9504cc1ed5d92c85e
Bobby Nelson <rnelsonxy@gmail.com>
2022-02-24 20:08:58 -0500

    Adds basic bus plotting functionality with leaflet.

--- BusTracker/app.R
+++ BusTracker/app.R
@@ -14,8 +14,18 @@ base_url <- "http://realtime.portauthority.org/bustime/api/v3/getvehicles"
 
 # function to query bus data from api
 # following from: https://urbandatapalette.com/post/2021-03-xml-dataframe-r/
-getBusData <- function(route) {
-    url <- paste0(base_url, "?key=", key, "&rt=", route)
+getBusData <- function(value, type) {
+    # add key to request
+    url <- paste0(base_url, "?key=", key)
+    
+    # add either route or vehicle id to request
+    if (type == "route") {
+        url <- paste0(url, "&rt=", value)
+    } else if (type == "vid") {
+        url <- paste0(url, "&vid=", value)
+    } else {
+        stop("Error, request type not recognized.")
+    }
     request <- RETRY("GET", URLencode(url), repeated=TRUE)
     content <- content(request, "text")
     
@@ -28,11 +38,27 @@ getBusData <- function(route) {
         readr::type_convert() 
 }
 
+# define custom icons for the map
+icons <- awesomeIconList(
+    bus.simple = makeAwesomeIcon(icon="bus-simple", library="fa")
+)
+
 # Define UI for application that draws a histogram
 ui <- navbarPage(
 
     # Application title
     titlePanel("Pittsburgh Bus Tracker"),
+    
+    # bus map
+    tabPanel("Live Map",
+             mainPanel(
+                 shinyjs::useShinyjs(),
+                 tags$style(type = "text/css", ".leaflet {height: calc(100vh - 90px) !important;}
+                                         body {background-color: #D4EFDF;}"),
+                 # Map Output
+                 leafletOutput("leaflet")
+                )
+             ),
     
     # top bar menu
     tabPanel("Data Table",
@@ -47,11 +73,35 @@ server <- function(input, output) {
     
     # updating vehicle location data
     bus.data <- reactive({
-        getBusData(2)
+        getBusData(value=2, type="route")
     })
 
     # raw data table for display
     output$table <- DT::renderDataTable(bus.data())
+    
+    # leaflet map base
+    output$leaflet <- renderLeaflet({
+        leaflet() %>%
+            addProviderTiles("OpenStreetMap.HOT") %>%
+            setView(-79.9959, 40.4406, 10)
+    })
+    
+    # bus coordinate view
+    # only update when bus data updates
+    observe({
+        data <- bus.data() %>%
+            mutate(lat = as.numeric(lat),
+                   lon = as.numeric(lon))
+        
+        leafletProxy("leaflet", data=data) %>%
+            clearGroup(group="busPosition") %>%
+            addAwesomeMarkers(lng=~lon,
+                              lat=~lat,
+                              icon=makeAwesomeIcon(icon="bus", library="fa"),
+                              popup=~vid,
+                              group="busPosition",
+                              layerId=~vid)
+    })
 }
 
 # Run the application 
